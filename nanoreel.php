37ee77ee905582608d5f4c22080ccbf11dae1e4c
Lua <lua.dev26@gmail.com>
2025-12-25 20:19:22 -0500

    chore: update author URI in plugin header.

--- nanoreel.php
+++ nanoreel.php
@@ -7,7 +7,7 @@
  * Requires at least: 5.8
  * Requires PHP: 7.4
  * Author: NanoReel
- * Author URI: https://nanoreel.vercel.app
+ * Author URI: https://github.com/LuaGR
  * License: GPL v2 or later
  * License URI: https://www.gnu.org/licenses/gpl-2.0.html
  * Text Domain: nanoreel
